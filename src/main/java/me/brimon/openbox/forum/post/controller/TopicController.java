b0871cfb8c6068358758ed330bf38b2fef4b1fc6
Lyric Wang <acgucheng@gmail.com>
2024-05-12 16:46:36 -0700

    Add security related codes

--- src/main/java/me/brimon/openbox/forum/post/controller/TopicController.java
+++ src/main/java/me/brimon/openbox/forum/post/controller/TopicController.java
@@ -1,10 +1,14 @@
 package me.brimon.openbox.forum.post.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/topic")
 public class TopicController {
 
+    @GetMapping
     public String getTopic() {
         return "Hello, World!";
     }
