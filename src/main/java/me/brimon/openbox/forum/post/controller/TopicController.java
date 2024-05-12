a5d94439dcb71cc2a2bc135ecaf6f2e76136aa65
古城秋月 <acgucheng@gmail.com>
2024-05-12 12:30:34 -0700

    Project initiated

---
+++ src/main/java/me/brimon/openbox/forum/post/controller/TopicController.java
@@ -0,0 +1,12 @@
+package me.brimon.openbox.forum.post.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+
+    public String getTopic() {
+        return "Hello, World!";
+    }
+
+}
---
+++ src/test/java/me/brimon/openbox/forum/ForumApplicationTests.java
@@ -0,0 +1,13 @@
+package me.brimon.openbox.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
